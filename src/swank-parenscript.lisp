92637a7da7c6f32e6e3491b75863551b4ceca6f4
Bart Botta <00003b@gmail.com>
2011-04-23 12:59:02 -0500

    use new variable names

--- src/swank-parenscript.lisp
+++ src/swank-parenscript.lisp
@@ -1,17 +1,17 @@
 (in-package :parenscript)
 
 (defun parenscript-function-p (symbol)
-  (and (or (gethash symbol *ps-macro-toplevel* )
-           (gethash symbol *ps-function-toplevel-cache*))
+  (and (or (gethash symbol *macro-toplevel-lambda-list* )
+           (gethash symbol *function-lambda-list*))
        t))
 #++
 (pushnew 'parenscript-function-p swank::*external-valid-function-name-p-hooks*)
 
 (defun parenscript-arglist (fname)
   (acond
-    ((gethash fname *ps-macro-toplevel-lambda-list*)
+    ((gethash fname *macro-toplevel-lambda-list*)
      (values it t))
-    ((gethash fname *ps-function-toplevel-cache*)
+    ((gethash fname *function-lambda-list*)
      (values it t))))
 #++
 (pushnew 'parenscript-arglist swank::*external-arglist-hooks*)
